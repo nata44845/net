6cd4615f0a0d884fe4b0053c2dbff5075d14c840
nata44845 <krppost@yandex.ru>
2023-01-15 13:34:22 +0700

    net5

---
+++ net5_HelloMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string name = Console.ReadLine();
+
+if (name == "Masha")
+{
+    Console.WriteLine("Ура это же Masha!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(name);
+}
