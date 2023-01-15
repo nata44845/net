467ae753f75e49f704930ee5caf353fb22b19ae9
nata44845 <krppost@yandex.ru>
2023-01-15 13:34:58 +0700

    net4

--- net4_div/Program.cs
+++ net4_div/Program.cs
@@ -1,7 +1,10 @@
 ﻿
-double num1 = 12;
+double num1 = new Random().Next(1,100);
 
-double num2 = 5;
+double num2 = new Random().Next(1,100);
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
 
 Console.Write("Частное = ");
 Console.WriteLine(num1/num2);
