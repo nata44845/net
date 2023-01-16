aece744c9059e10f257505a9099e9d1d005bf745
nata44845 <krppost@yandex.ru>
2023-01-16 16:46:06 +0700

    task5

---
+++ sem1/task4_nummin/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+int i = -num;
+
+while (i <= num)
+
+{
+    Console.Write(i);
+    if (i < num) Console.Write(", ");
+    i = i + 1;
+}
+
---
+++ sem1/task5_last/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+int res = num%10; 
+
+Console.Write("Третья цифра "+res);
