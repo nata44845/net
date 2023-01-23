458014d760b8669af0cc4010d16e7bc5a9b3de1e
nata44845 <krppost@yandex.ru>
2023-01-23 17:25:59 +0700

    l3

---
+++ l3/net1_meth/Program.cs
@@ -0,0 +1,39 @@
+﻿//Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+
+//Method1();
+
+//Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine("Автор " + msg);
+}
+
+//Method2("Иванов");
+
+
+//Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+// int year=Method3();
+// Console.WriteLine(year);
+
+//Вид 4
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i=0; i<count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res=Method4(10,"hi ");
+Console.WriteLine(res);
---
+++ l3/net5_sortmax/Program.cs
@@ -0,0 +1,38 @@
+﻿//Упорядочить массив по убыванию
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+}
+
+
+void SelectionSort(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count-1; i++)
+    {
+        int max = i;
+        for (int j = i + 1; j < count; j++)
+        {
+            if (array[j] > array[max]) max = j;
+        }
+        int temp = array[i];
+        array[i] = array[max];
+        array[max] = temp;
+    }
+    PrintArray(array);
+}
+
+
+int[] arr = { 6, 8, 3, 2, 1, 4, 5, 7 };
+
+PrintArray(arr);
+
+Console.WriteLine();
+
+SelectionSort(arr);
