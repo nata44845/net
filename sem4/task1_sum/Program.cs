b9e9b2971ee19eeb22042793e03e3015231df3b7
nata44845 <krppost@yandex.ru>
2023-01-24 16:06:24 +0700

    sem4

---
+++ sem4/task1_sum/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int getNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int getSum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+//Ввод данных от пользователя
+int number = getNum("Введите число: ");
+
+//Найти сумму чисел
+int summa = getSum(number);
+
+Console.WriteLine($"{number} -> {summa}");
---
+++ sem4/task3_proizv/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int getNum(string text)
+{
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int getCompose(int num)
+{
+    int com = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        com = com * i;
+    }
+    return com;
+}
+
+//Ввод данных от пользователя
+int number = getNum("Введите число: ");
+
+//Найти сумму чисел
+int compose = getCompose(number);
+
+Console.WriteLine($"{number} -> {compose}");
