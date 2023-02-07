890947c34abae7b6d809db7eee77f48f4eca097c
nata44845 <krppost@yandex.ru>
2023-02-07 11:34:04 +0700

    l6

---
+++ l6/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (5,6)"
+        .Replace("(", "")
+        .Replace(")", "");
+
+var data = text.Split(" ")
+        .Select(item => item.Split(","))
+        .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+        .Where(e => e.x % 2 == 0)
+        .Select(point => (point.x * 10, point.y))
+        .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
+
+///<summary>Метод</summary>
+///<param name="n">Параметр</param>
+///<returns>Возвращает</returns>
+
